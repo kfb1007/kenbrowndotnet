a2c64a4eb9f7b418d843f49cfb10bd3bd3e6956d
Ken <email@kenbrown.net>
2026-01-28 18:50:36 -0500

    add

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Ken Brown";
+export const SITE_DESCRIPTION = "Helping to build trust in AI. CISO and cybersecurity leader with 22+ years experience.";
